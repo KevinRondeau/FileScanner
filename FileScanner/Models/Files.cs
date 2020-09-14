b5af714cc2f48673800c19ba808d9a644e7bc92a
KevinRondeau <k.rondeau1991@hotmail.com>
2020-09-14 14:47:30 -0400

    initial comit with icons

---
+++ FileScanner/Models/Files.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FileScanner.Models
+{
+    public class Files
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+
+
+        public Files(string P)
+        {
+            Name = P;
+            if (checkDir(P) == false)
+            {
+                Image = "/file.png";
+            }
+            if (checkDir(P) == true)
+            {
+                Image = "/folder.jpg";
+            }
+
+        }
+        private Boolean checkDir(string n)
+        {
+            // get the file attributes for file or directory
+            FileAttributes attr = File.GetAttributes(n);
+
+            if (attr.HasFlag(FileAttributes.Directory))
+                return true;
+            else
+                return false;
+
+
+            ////--original method---////
+            //// get the file attributes for file or directory
+            //FileAttributes attr = File.GetAttributes(@"c:\Temp");
+
+            //if (attr.HasFlag(FileAttributes.Directory))
+            //    MessageBox.Show("Its a directory");
+            //else
+            //    MessageBox.Show("Its a file");
+        }
+
+    }
+
+    
+}
--- FileScanner/ViewModels/MainViewModel.cs
+++ FileScanner/ViewModels/MainViewModel.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
+using System.Drawing;
+using FileScanner.Models;
 
 namespace FileScanner.ViewModels
 {
@@ -14,7 +16,7 @@ namespace FileScanner.ViewModels
     {
         private string selectedFolder;
         private ObservableCollection<string> folderItems = new ObservableCollection<string>();
-         
+        private ObservableCollection<Files> myFiles=new ObservableCollection<Files>();
         public DelegateCommand<string> OpenFolderCommand { get; private set; }
         public DelegateCommand<string> ScanFolderCommand { get; private set; }
 
@@ -26,7 +28,15 @@ namespace FileScanner.ViewModels
                 OnPropertyChanged();
             }
         }
-
+        public ObservableCollection<Files> MyFiles
+        {
+            get => myFiles;
+            set
+            {
+                myFiles = value;
+                OnPropertyChanged();
+            }
+        }
         public string SelectedFolder
         {
             get => selectedFolder;
@@ -62,18 +72,26 @@ namespace FileScanner.ViewModels
 
         private void ScanFolder(string dir)
         {
+            MyFiles.Clear();
             FolderItems = new ObservableCollection<string>(GetDirs(dir));
+            
             
             foreach (var item in Directory.EnumerateFiles(dir, "*"))
             {
                 FolderItems.Add(item);
+                MyFiles.Add(new Files(item));
+
             }
+           
         }
 
         IEnumerable<string> GetDirs(string dir)
-        {            
+        {
+     
+
             foreach (var d in Directory.EnumerateDirectories(dir, "*"))
             {
+                MyFiles.Add(new Files(d));
                 yield return d;
             }
         }
